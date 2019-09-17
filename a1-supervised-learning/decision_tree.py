5505d6c60b31fbc73566e7b785917e7799207d97
changrybirds <changrybirds@users.noreply.github.com>
2019-09-17 07:07:28 -0700

    Add learning curve code

--- a1-supervised-learning/decision_tree.py
+++ a1-supervised-learning/decision_tree.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split, learning_curve
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn import metrics
 import matplotlib.pyplot as plt
@@ -20,13 +20,95 @@ def encode_data(df, cols):
 
     oh_enc = OneHotEncoder()
 
-    # train/test split
 
-    # cross validation
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    # adapted from sklearn documentation:
+    # https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
 
-    # fit
+    Generate a simple plot of the test and training learning curve.
 
-if __name__ == "__main__":
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+def main():
     seed_val = 313
     abalone_names = [
         'sex', 'length', 'diameter', 'height', 'whole_weight',
@@ -49,7 +131,8 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=seed_val)
 
     # instantiate decision tree
-    dtclf = DecisionTreeClassifier()
+    max_depth = np.arange(3, 7)
+    dtclf = DecisionTreeClassifier(max_depth=max_depth[0])
 
     outputs = dtclf.fit(X_train, y_train)
 
@@ -57,5 +140,10 @@ if __name__ == "__main__":
     scores = cross_val_score(dtclf, X_train, y=y_train)
 
     # calculate and print learning curves
+    train_sizes = [0.2, 0.4, 0.6, 0.8]
+    train_sizes_abs, train_scores, test_scores = learning_curve(dtclf, X_train, y_train, train_sizes=train_sizes)
+    mean_train_scores = train_scores.mean()
 
-    print(scores)
+
+if __name__ == "__main__":
+    main()
