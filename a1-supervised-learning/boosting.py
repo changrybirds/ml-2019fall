3bd37c9fbc8b29ac61c153bd167c9e30b2f94a0d
changrybirds <changrybirds@users.noreply.github.com>
2019-09-22 12:36:34 -0700

    Debugging

--- a1-supervised-learning/boosting.py
+++ a1-supervised-learning/boosting.py
@@ -9,11 +9,11 @@ import dataset_processing as data_proc
 
 
 def model_complexity_curve(estimator, X_train, y_train, hp, hp_vals, cv=None):
-    df = pd.DataFrame(index=hidden_units, columns=['train', 'cv'])
+    df = pd.DataFrame(index=hp_vals, columns=['train', 'cv'])
 
     for hp_val in hp_vals:
         kwargs = {
-            'estimator': estimator,
+            'base_estimator': estimator,
             hp: hp_val,
             'random_state': data_proc.SEED_VAL}
 
@@ -44,7 +44,7 @@ def run_experiment(dt_estimator, dataset_name, X_train, X_test, y_train, y_test,
         print(hp_vals)
 
     n_estimators_mc = model_complexity_curve(
-        X_train, y_train, hp, hp_vals, cv=data_proc.CV_VAL)
+        dt_estimator, X_train, y_train, hp, hp_vals, cv=data_proc.CV_VAL)
     n_estimators_hp = n_estimators_mc['cv'].idxmax()
     if verbose:
         print(n_estimators_mc.head(10))
@@ -63,11 +63,11 @@ def run_experiment(dt_estimator, dataset_name, X_train, X_test, y_train, y_test,
 
     # calculate model complexity scores for learning_rate
     hp = 'learning_rate'
-    hp_vals = np.logspace(-3, 1, base=10.0, num=6)  # this should vary for each hyperparameter
-    learning_rate_mc = data_proc.plot_learning_curve(
-        X_train, y_train, hp, hp_vals, cv=data_proc.CV_VAL)
-    learning_rate_hp = learning_rate_mc['cv'].idxmax()
+    hp_vals = np.logspace(-3, 1, base=10.0, num=5)  # this should vary for each hyperparameter
 
+    learning_rate_mc = model_complexity_curve(
+        dt_estimator, X_train, y_train, hp, hp_vals, cv=data_proc.CV_VAL)
+    learning_rate_hp = learning_rate_mc['cv'].idxmax()
     if verbose:
         print(learning_rate_mc.head(10))
     if verbose:
@@ -76,7 +76,6 @@ def run_experiment(dt_estimator, dataset_name, X_train, X_test, y_train, y_test,
     data_proc.plot_model_complexity_charts(
         learning_rate_mc['train'], learning_rate_mc['cv'],
         dataset_name + ': MCC for ' + hp, hp, xscale_type='log')
-
     if show_plots:
         plt.show()
 
@@ -89,14 +88,11 @@ def run_experiment(dt_estimator, dataset_name, X_train, X_test, y_train, y_test,
         dt_estimator, n_estimators=n_estimators_hp, learning_rate=learning_rate_hp,
         random_state=data_proc.SEED_VAL)
 
-    adaclf.fit(X_train, y_train)
-
     # calculate and print learning curves
     train_sizes = np.linspace(0.1, 0.9, 9)
     data_proc.plot_learning_curve(
         adaclf, dataset_name + ': learning curves',
         X_train, y_train, cv=data_proc.CV_VAL, train_sizes=train_sizes)
-
     if show_plots:
         plt.show()
 
@@ -104,25 +100,27 @@ def run_experiment(dt_estimator, dataset_name, X_train, X_test, y_train, y_test,
     plt.clf()
     plt.close()
 
+    adaclf.fit(X_train, y_train)
+
     train_score = data_proc.model_train_score(adaclf, X_train, y_train)
     test_score = data_proc.model_test_score(adaclf, X_test, y_test)
     print("AdaBoostClassifier training set score for " + dataset_name + ": ", train_score)
     print("AdaBoostClassifier holdout set score for " + dataset_name + ": ", test_score)
 
 
-def abalone(verbose=False, show_plots=False):
+def abalone(dt_estimator, verbose=False, show_plots=False):
     X_train, X_test, y_train, y_test = data_proc.process_abalone()
 
     run_experiment(
-        'abalone', X_train, X_test, y_train, y_test,
+        dt_estimator, 'abalone', X_train, X_test, y_train, y_test,
         verbose=verbose, show_plots=show_plots)
 
 
-def online_shopping(verbose=False, show_plots=False):
+def online_shopping(dt_estimator, verbose=False, show_plots=False):
     X_train, X_test, y_train, y_test = data_proc.process_online_shopping()
 
     run_experiment(
-        'online_shopping', X_train, X_test, y_train, y_test,
+        dt_estimator, 'online_shopping', X_train, X_test, y_train, y_test,
         verbose=verbose, show_plots=show_plots)
 
 
